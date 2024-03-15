004baa587386130fa8485d86efdb2d16ad6ccec1
Xbostick <borovkov-pavel@yandex.ru>
2024-03-15 14:55:20 +0300

    modified:   src/main.cpp 	modified:   .vscode/settings.json 	modified:   pages/index.html 	modified:   platformio.ini 	modified:   src/pages.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
+#include <WebSocketsServer.h>
 #include <FastLED.h>
 #include "string.h"
 #include "pages.h"
@@ -11,26 +12,46 @@
 #include "secure_data.h"
 
 FASTLED_USING_NAMESPACE
-#define DEBUG 1
+
 #define NUM_LEDS1 130
 #define NUM_LEDS2 200
-#define NOISE_PIN A0
 #define LED_PIN1 D2
 #define LED_PIN2 D3
+#define LED 16 // led connected to D0
+
+#define DistanceSensorTrigPin D5
+#define DistanceSensorEchoPin D6
+#define DistanceSensorInterval_ms 500
+#define DistanceSensorPause_ms 5000
+#define DistanceSensorDistance_sm 7
+unsigned long DistanceSensorTimeTicker;
+unsigned long DistanceSensorTimeDelay;
 
 bool ServerLedState = 0;
 bool IS_ON = 0;
 bool RDY2USE = 1;
 bool SAFEMODE = 1;
 bool NEED_REFRESH = 0;
+
 uint8_t count = 0;
-uint32_t main_color = 0x00;
+uint32_t main_color = 0xFF;
+unsigned long ActualCycleTime;
 
 CRGB leds1[NUM_LEDS1];
 CRGB leds2[NUM_LEDS2];
 
+#define DEBUG 0
+/* All debug defines here*/
 
-int LED = 16; // led connected to D0
+void debug_setup(){
+  ;
+} 
+
+void debug_loop(){
+ ;
+}
+
+/*end*/
 
 
 ESP8266WebServer HttpServer(SERVERPORT);
@@ -106,6 +127,7 @@ void setup_server(const char* ssid, const char* password){
     HttpServer.begin();
 }
 
+
 void server_loop(){
     HttpServer.handleClient();
 }
@@ -126,6 +148,34 @@ void FastLED_loop(){
     }
 }
 
+void DistanseSensor_setup(){
+  pinMode(DistanceSensorTrigPin, OUTPUT);
+  pinMode(DistanceSensorEchoPin, INPUT); 
+}
+
+void DistanceSensor_loop(){
+  if ((ActualCycleTime - DistanceSensorTimeTicker > DistanceSensorInterval_ms) 
+  and (ActualCycleTime - DistanceSensorTimeDelay  > DistanceSensorPause_ms)){
+    DistanceSensorTimeTicker = ActualCycleTime;
+    digitalWrite(DistanceSensorTrigPin, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(DistanceSensorTrigPin, LOW);
+
+    float distance = ( pulseIn(DistanceSensorEchoPin, HIGH)*.0343)/2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+
+    if (distance < DistanceSensorDistance_sm){
+       IS_ON = !IS_ON;
+       NEED_REFRESH = 1;
+       Serial.print("power change");
+       DistanceSensorTimeDelay =  ActualCycleTime;
+      }
+  }
+    
+}
+
+
 bool IsReadyToUse_check_by_EEPROM(){
   EEPROM.begin(8);
   uint8_t restart_num = EEPROM.read(0);
@@ -133,7 +183,12 @@ bool IsReadyToUse_check_by_EEPROM(){
   Serial.println(restart_num);
   EEPROM.write(0,restart_num+1);
   EEPROM.commit();
-  return restart_num < 3;
+  return ((restart_num < 3) or DEBUG);
+}
+
+void refresh_timers(){
+  DistanceSensorTimeTicker = 0;
+  DistanceSensorTimeDelay = 0;
 }
 
 void DisableSafeMode(){
@@ -166,6 +221,9 @@ void setup(){
     pinMode(LED_PIN2,OUTPUT);
   };
 
+  if (RDY2USE) DistanseSensor_setup();
+  if (DEBUG) debug_setup();
+
   digitalWrite(LED,0);
 }
 
@@ -175,9 +233,10 @@ void loop(){
 
   if (RDY2USE){
     FastLED_loop();
+    DistanceSensor_loop();
   }
-  
-  if ((millis() > 30000) and SAFEMODE) {
+
+  if ((ActualCycleTime > 30000) and SAFEMODE) {
     SAFEMODE = false;
     digitalWrite(LED,1);
     delay(500);
@@ -185,4 +244,12 @@ void loop(){
     DisableSafeMode();
   };
     
+  if (DEBUG) debug_loop();
+
+  if (ActualCycleTime > millis())
+  {
+    refresh_timers();
+  }
+
+  ActualCycleTime = millis();
 }
